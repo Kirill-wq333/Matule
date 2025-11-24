4c4645ccc81aa8510540187e3d48bdd015b3b0fa
Kirill-wq333 <154874212+Kirill-wq333@users.noreply.github.com>
2025-11-24 20:22:45 +0400

    The main app module has been fixed and new components have been added: Popular, Catalog, Arrivals or promotions and side menus

--- app/src/main/java/com/example/matule/ui/manage/AppStartManager.kt
+++ app/src/main/java/com/example/matule/ui/manage/AppStartManager.kt
@@ -11,26 +11,18 @@ class AppStartManager @Inject constructor(
     private val tokenProvider: TokenProvider,
     private val appPreferencesRepository: AppPreferencesImpl
 ) {
-
     suspend fun getStartDestination(): String {
         val appPreferences = appPreferencesRepository.getAppPreferences()
         val token = tokenProvider.getToken()
 
-        println("🔍 AppStartManager - Token: $token")
-        println("🔍 AppStartManager - Onboarding completed: ${appPreferences.isOnboardingCompleted}")
-        println("🔍 AppStartManager - Is logged in: ${appPreferences.isLoggedIn}")
-
         return when {
             !appPreferences.isOnboardingCompleted -> {
-                println("🚀 Start: ONBOARDING (first launch)")
                 AppRouts.ONBOARDING
             }
             token != null && appPreferences.isLoggedIn -> {
-                println("🚀 Start: MAIN (user logged in)")
                 AppRouts.MAIN
             }
             else -> {
-                println("🚀 Start: AUTH (user not logged in)")
                 AppRouts.AUTH
             }
         }
